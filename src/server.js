b3c515425dd9f14adbed3767dc62e5322afe66e9
Farhan-Shaikh-786 <farhanshaikh25.fs@gmail.com>
2025-07-29 20:29:28 +0530

    Frontend url

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 
 const allowedOrigin = [
     "http://localhost:5173",
-    "https://todoApp.netlify.app"
+    "https://todoappweh.netlify.app"
 ]
 app.use(cors({
     origin: (origin, callback) => {
