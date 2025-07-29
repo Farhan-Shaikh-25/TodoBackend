0367c8a54064368bf9d01683a0ccab84f2618f4c
Farhan-Shaikh-786 <farhanshaikh25.fs@gmail.com>
2025-07-29 21:51:12 +0530

    reordered

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,11 +4,19 @@ import bcrypt from "bcrypt"
 import AuthCheck from "../middlewares/authCheck.js"
 export const userRoute = express.Router() 
 
-userRoute.get("/:userId",AuthCheck,async(req,res)=>{
-    const user = await User.findOne({_id : req.params.userId}).select("userName")
-    res.json({"userName":user.userName})
+userRoute.post("/signup/",async (req,res)=>{
+    const userExists = await User.findOne(
+        {userName: req.body.userName}
+    )
+    let resp = ""
+    if(!userExists) {
+        const newUser = new User({userName:req.body.userName, userPass: req.body.userPass})
+        await newUser.save()
+        resp = "user created"
+    }
+    else resp = "user already exists!"
+    res.json({"message": resp})
 })
-
 
 userRoute.post("/login/", async(req, res) => {
     const userExists = await User.findOne(
@@ -28,17 +36,13 @@ userRoute.post("/login/", async(req, res) => {
     
 })
 
-userRoute.post("/signup/",async (req,res)=>{
-    const userExists = await User.findOne(
-        {userName: req.body.userName}
-    )
-    let resp = ""
-    if(!userExists) {
-        const newUser = new User({userName:req.body.userName, userPass: req.body.userPass})
-        await newUser.save()
-        resp = "user created"
-    }
-    else resp = "user already exists!"
-    res.json({"message": resp})
+userRoute.get("/:userId",AuthCheck,async(req,res)=>{
+    const user = await User.findOne({_id : req.params.userId}).select("userName")
+    res.json({"userName":user.userName})
 })
+
+
+
+
+
 
